6c9dc965be79e5bd49ed61d9b77cbcbc9b1150ef
Kenneth Lorthioir <limb@Kenneth-Lorthioirs-MacBook-Pro.local>
2010-01-05 23:02:54 -0500

    completed clock update

--- clock.h
+++ clock.h
@@ -115,5 +115,122 @@ void clockSet(byte hour, byte min)
 
 void clockUpdate(byte hour, byte min)
 {
-  Tlc.update();
+  byte pinHour = 0;
+  byte mins = min%5;
+  
+  if(hour > 12)
+  {
+    hour -= 12;
+    pinHour = hour + 16;
+  }
+  else
+    pinHour = hour + 16;
+
+  if(mins != 0)
+  {
+    if(mins == 1)
+      fadeIn(pinMin1);
+    else if(mins == 2)
+      fadeIn(pinMin2);
+    else if(mins == 3)
+      fadeIn(pinMin3);
+    else if(mins == 4)
+      fadeIn(pinMin4);
+  }else switch((int)floor(min/5))
+  {
+    case 0:
+    {
+      byte in[] = {pinHour, pinOClock};
+      byte out[3];
+      if(hour == 0)
+      {
+        out[0] = pinEleven; 
+        out[1] = pinFiveTop;
+        out[2] = pinTo;
+      }
+      else
+      {
+        out[0] = pinHour - 1;
+        out[1] = pinFiveTop;
+        out[2] = pinTo;
+      }
+      fadeInOut(in, 2, out, 3);
+      break;
+    }
+    case 1:
+    {
+      byte in[] = {pinFiveTop, pinPast};
+      byte out[] = {pinOClock};
+      fadeInOut(in, 2, out, 1);
+      break;
+    }
+    case 2:
+    {
+      byte in[] = {pinTenTop};
+      byte out[] = {pinFiveTop};
+      fadeInOut(in, 1, out, 1);
+      break;
+    }
+    case 3:
+    {
+      byte in[] = {pinA, pinQuarter};
+      byte out[] = {pinTenTop};
+      fadeInOut(in, 2, out, 1);
+      break;
+    }
+    case 4:
+    {
+      byte in[] = {pinTwenty};
+      byte out[] = {pinA, pinQuarter};
+      fadeInOut(in, 1, out, 2);
+      break;
+    }
+    case 5:
+    {
+      fadeIn(pinFiveTop);
+      break;
+    }
+    case 6:
+    {
+      byte in[] = {pinHalf};
+      byte out[] = {pinTwenty, pinFiveTop};
+      fadeInOut(in, 1, out, 2);
+      break;
+    }
+    case 7:
+    {
+      byte in[] = {pinTwenty, pinFiveTop, pinTo};
+      byte out[] = {pinHalf, pinPast};
+      fadeInOut(in, 3, out, 2);
+      break;
+    }
+    case 8:
+    {
+      fadeOut(pinFiveTop);
+      break;
+    }
+    case 9:
+    {
+      byte in[] = {pinA, pinQuarter};
+      byte out[] = {pinTwenty};
+      fadeInOut(in, 2, out, 1);
+      break;
+    }
+    case 10:
+    {
+      byte in[] = {pinTenTop};
+      byte out[] = {pinA, pinQuarter};
+      fadeInOut(in, 1, out, 2);
+      break;
+    }
+    case 11:
+    {
+      byte in[] = {pinFiveTop};
+      byte out[] = {pinTenTop};
+      fadeInOut(in, 1, out, 1);
+      break;
+    }
+    default:
+      break;
+  }
 }
--- fade.h
+++ fade.h
@@ -11,3 +11,44 @@ void fadeInBoot(byte in[], int size, byte mins)
     delay(15);
   }
 }
+
+void fadeInOut(byte in[], int sizeIn, byte out[], byte sizeOut)
+{
+  int b = 4095;
+  for(int a = 0; a <= 4095; a += 35)
+  {
+    Tlc.set(pinItIs, a);
+    for(int n = 0; n < sizeIn; n++)
+      Tlc.set(in[n], a);
+    for(int n = 0; n < sizeOut; n++)
+      Tlc.set(out[n], b);
+    Tlc.set(pinMin1, b);
+    Tlc.set(pinMin2, b);
+    Tlc.set(pinMin3, b);
+    Tlc.set(pinMin4, b);
+    Tlc.update();
+    b -= 35;
+    delay(15);
+  }
+}
+
+
+void fadeIn(byte in)
+{
+  for(int a = 0; a <= 4095; a += 35)
+  {
+    Tlc.set(in, a);
+    Tlc.update();
+    delay(15);
+  }
+}
+
+void fadeOut(byte out)
+{
+  for(int a = 4095; a >= 0; a -= 35)
+  {
+    Tlc.set(out, a);
+    Tlc.update();
+    delay(15);
+  }
+}
